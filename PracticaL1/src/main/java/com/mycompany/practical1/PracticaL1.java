2cfc836edba753c9bf43d1debad0e6ce8f520600
Diego <diegocayax202231074@cunoc.edu.gt>
2024-08-23 17:52:04 -0600

    diseño del menu

---
+++ PracticaL1/src/main/java/com/mycompany/practical1/PracticaL1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practical1;
+
+/**
+ *
+ * @author rafael-cayax
+ */
+public class PracticaL1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
